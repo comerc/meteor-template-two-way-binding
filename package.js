dd82c2b1f20d660a282e8d8023f73655fd5df786
comerc <comerc@mail.ru>
2016-05-03 04:37:55 +0300

    Version 1.3.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'comerc:template-two-way-binding',
-  version: '1.3.2',
+  version: '1.3.3',
   summary: 'Two-Way Binding for Blaze templates',
   git: 'https://github.com/comerc/meteor-template-two-way-binding',
   documentation: 'README.md'
@@ -8,11 +8,11 @@ Package.describe({
 
 Package.onUse(function (api) {
   // Have to stay on Meteor 1.2.1 to be compatible with all Meteor versions.
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('1.3.2');
   api.use([
     'templating',
     'blaze-html-templates',
-    'aldeed:template-extension@4.0.0',
+    'aldeed:template-extension@4.0.0', // TODO how to support METEOR@1.2.1
   ]);
   api.addFiles('template-two-way-binding.js', 'client');
   api.export('TemplateTwoWayBinding');
